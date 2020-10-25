3699083742d8aef8a4fb968b98942b2e3c63c746
lovit <soy.lovit@gmail.com>
2020-10-26 01:51:55 +0900

    Version change: 0.3.0 (#9)

--- namuwikitext/about.py
+++ namuwikitext/about.py
@@ -1,5 +1,5 @@
 __author__ = 'lovit'
 __name__ = 'Namuwikitext'
 __license__ = "CC BY-NC-SA 2.0 KR \nReference: https://creativecommons.org/licenses/by-nc-sa/2.0/kr/"
-__version__ = '0.1.0'
+__version__ = '0.3.0'
 
